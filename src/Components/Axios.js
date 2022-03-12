8aa1998fdbd05f8e9d938d2aacf50500fec0af1d
Borhan Uddin <borhaninfos@gmail.com>
2022-03-12 12:51:25 +0600

    changes

--- src/Components/Axios.js
+++ src/Components/Axios.js
@@ -3,11 +3,27 @@ import axios from "axios";
 
 export default class AxiosAPi extends Component {
   componentDidMount() {
+    //   axios
+    //     .get("http://localhost:3001/dishes")
+    //     .then((res) => res.data)
+    //     .then((data) => console.log(data))
+    //     .catch((error) => console.log(error.message));
+    //
+
+    // axios
+    //   .post("http://localhost:3001/dishes", { name: "Pizza", price: "799" })
+    //   .then((res) => console.log(res));
+
+    // axios
+    //   .put("http://localhost:3001/dishes/2", {
+    //     name: "Chiken Fry",
+    //     price: "250",
+    //   })
+    //   .then((res) => console.log(res));
+
     axios
-      .get("http://localhost:3001/dishes")
-      .then((res) => res.data)
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error.message));
+      .delete("http://localhost:3001/dishes/4", {})
+      .then((res) => console.log(res));
   }
 
   render() {
